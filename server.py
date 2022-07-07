37dfc194caad516c862774760c9ead253833546d
abunuwas <joseharoperalta@gmail.com>
2022-07-07 10:21:29 +0100

    Parametrize session factory and repositories

--- server.py
+++ server.py
@@ -3,7 +3,9 @@ from fastapi import FastAPI
 from api import router
 
 
-def create_server():
+def create_server(session_maker=None, repositories=None):
     server = FastAPI(debug=True)
     server.include_router(router)
+    server.session_maker = session_maker
+    server.repositories = repositories
     return server
