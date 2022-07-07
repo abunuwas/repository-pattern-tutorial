47230ba19028be48b74f18d5289a6b086187d5e7
abunuwas <joseharoperalta@gmail.com>
2022-07-07 10:22:12 +0100

    Initialize session factory and repositories registry in runner script

--- run.py
+++ run.py
@@ -1,3 +1,14 @@
+import os
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from data_access.repositories_registry import RepositoriesRegistry
+from data_access.repository import BookingsRepository
 from server import create_server
 
-server = create_server()
+session_maker = sessionmaker(bind=create_engine(os.getenv("DB_URL")))
+
+repositories_registry = RepositoriesRegistry(bookings_repo=BookingsRepository)
+
+server = create_server(session_maker=session_maker, repositories=repositories_registry)
